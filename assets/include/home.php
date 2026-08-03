2522eb990b27e229feff2fb453ee6ef69dfbc919
David Marques <davidmarquesx@bol.com.br>
2026-08-03 13:01:49 -0300

    Update home.php

--- assets/include/home.php
+++ assets/include/home.php
@@ -1,17 +1,18 @@
 <div class="container">
-   <div class="row">
+  <!-- <div class="row">
         <div class="main-image col-md-6">
-           <!-- <image > -->
+           <!-- <image >
         </div>
-        <div class="main-text col-md-6">
-            <div id="info">
+       
+    </div> -->
+    <div class="main-text col-md-6">
+        <div id="info">
                 <p>Olá! Sou</p>
                 <h1>David <br>
                    Marques
                 </h1>
                 <h2>Designer e front-end developer, focado em desenvolvimento de <strong> projetos gráficos</strong> e <strong>digitais</strong>.</h2>
                 <h3>Seja bem-vindo!</h3>
-            </div>
-        </div>
-    </div>
+       </div>
+   </div>
 </div>
